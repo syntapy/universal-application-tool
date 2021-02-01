de9f46947928baf0febac2a363175c64143fe02d
Sami Abdul-Wahid <syntapy@pm.me>
2021-01-31 16:37:24 -0800

    1st attempt at terraform deployment (#10)

--- main.tf
+++ main.tf
@@ -1,39 +1,104 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "2.10.0"
+# Filename: main.tf
+# Configure GCP project
+provider "google" {
+  credentials = file("credentials.json")
+  project = var.project_id 
+}
+# Deploy image to Cloud Run
+resource "google_cloud_run_service" "uat" {
+  name     = "uat"
+  location = "us-west1"
+  template {
+    spec {
+      containers {
+        image = join("", ["gcr.io/", var.project_id, "/uat"])
+        #ports {
+        #  container_port = 9000
+        #}
+        resources {
+          limits = {memory = "2048M"}
+        }
+      }
     }
   }
-}
-
-# Configure Docker provider and connect to the local Docker socket
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
-
-# Containers
-resource "docker_container" "postgres" {
-  image = docker_image.postgres.latest
-  name  = "db"
-  restart = "always"
-  env = ["POSTGRES_PASSWORD=example"]
-}
-
-resource "docker_container" "adminer" {
-  image = docker_image.adminer.latest
-  name = "adminer"
-  restart = "always"
-  ports {
-    internal = 8080
-    external = 8080
+  traffic {
+    percent         = 100
+    latest_revision = true
   }
 }
 
-resource "docker_image" "postgres" {
-  name = "postgres:latest"
+resource "google_cloud_run_service" "postgres" {
+  name     = "postgres"
+  location = "us-west1"
+  template {
+    spec {
+      containers {
+        image = join("", ["gcr.io/", var.project_id, "/postgres"])
+        env {
+          name = "POSTGRES_PASSWORD"
+          value = "example"
+        }
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
 }
 
-resource "docker_image" "adminer" {
-  name = "adminer:latest"
+resource "google_cloud_run_service" "adminer" {
+  name     = "adminer"
+  location = "us-west1"
+  template {
+    spec {
+      containers {
+        image = join("", ["gcr.io/", var.project_id, "/adminer"])
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Create public access
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+# Enable public access on Cloud Run service
+resource "google_cloud_run_service_iam_policy" "uat_auth" {
+  location    = google_cloud_run_service.uat.location
+  project     = google_cloud_run_service.uat.project
+  service     = google_cloud_run_service.uat.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+resource "google_cloud_run_service_iam_policy" "adminer_auth" {
+  location    = google_cloud_run_service.adminer.location
+  project     = google_cloud_run_service.adminer.project
+  service     = google_cloud_run_service.adminer.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+resource "google_cloud_run_service_iam_policy" "postgres_auth" {
+  location    = google_cloud_run_service.postgres.location
+  project     = google_cloud_run_service.postgres.project
+  service     = google_cloud_run_service.postgres.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+# Return service URL
+output "url_uat" {
+  value = google_cloud_run_service.uat.status[0].url
+}
+
+output "url_adminer" {
+  value = google_cloud_run_service.adminer.status[0].url
 }
